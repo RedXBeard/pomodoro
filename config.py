b5c27e8191a89d88cd2cdd5187d044cb6f7a1615
Barbaros Yildirim <barbaros.yildirim@markafoni.com>
2014-12-11 11:32:48 +0200

    visual effects added. info for completed pomodoro taken, break and continue buttons added with actions.

--- config.py
+++ config.py
@@ -1,25 +1,50 @@
 import os
 import kivy
+from subprocess import Popen, PIPE
 from kivy.core.text import LabelBase
 from kivy.utils import get_color_from_hex
+from kivy.storage.jsonstore import JsonStore
+
+def run_syscall(cmd):
+    """
+    run_syscall; handle sys calls this function used as shortcut.
+
+    ::cmd: String, shell command is expected.
+    """
+    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+    return out.rstrip()
 
 PATH_SEPERATOR = '\\' if os.path.realpath(__file__).find('\\') != -1 else '/'
 
-PROJECT_PATH = PATH_SEPERATOR.join(os.path.realpath(__file__).\
-                                    split(PATH_SEPERATOR)[:-1])
+PROJECT_PATH = PATH_SEPERATOR.join(os.path.realpath(__file__).
+                                   split(PATH_SEPERATOR)[:-1])
 
-WORK_TIME_PERIOD = 30
-BREAK_TIME_PERIOD = 10
+if PATH_SEPERATOR == '/':
+    cmd = "echo $HOME"
+else:
+    cmd = "echo %USERPROFILE%"
+
+out = run_syscall(cmd)
+DATAFILE = "%(out)s%(ps)s.kivypomomdoro%(ps)spomodoro" % {'out': out.rstrip(),
+                                                          'ps': PATH_SEPERATOR}
+DB = JsonStore(DATAFILE)
+directory = os.path.dirname(DATAFILE)
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+WORK_TIME_PERIOD = 1500
+BREAK_TIME_PERIOD = 300
 
 KIVY_FONTS = [{
-        "name": "WebAwesome",
-        "fn_regular": "%(pp)s%(ps)sassets%(ps)sfontawesome-webfont.ttf"%{'pp':PROJECT_PATH,
-                                                                         'ps':PATH_SEPERATOR},
-    },{
-        "name": "FiraSans",
-        "fn_regular": "%(pp)s%(ps)sassets%(ps)sFiraSans-Regular.ttf"%{'pp':PROJECT_PATH,
-                                                                         'ps':PATH_SEPERATOR},
-    }]
+    "name": "WebAwesome",
+    "fn_regular": "%(pp)s%(ps)sassets%(ps)sfontawesome-webfont.ttf" % {'pp': PROJECT_PATH,
+                                                                       'ps': PATH_SEPERATOR},
+}, {
+    "name": "FiraSans",
+    "fn_regular": "%(pp)s%(ps)sassets%(ps)sFiraSans-Regular.ttf" % {'pp': PROJECT_PATH,
+                                                                    'ps': PATH_SEPERATOR},
+}]
 
 
 for font in KIVY_FONTS:
@@ -28,11 +53,13 @@ for font in KIVY_FONTS:
 KIVY_VERSION = kivy.__version__
 
 KIVY_DEFAULT_FONT = "FiraSans"
-KIVY_DEFAULT_FONT_PATH = filter(lambda x: x['name'] == KIVY_DEFAULT_FONT, KIVY_FONTS)[0]['fn_regular']
+KIVY_DEFAULT_FONT_PATH = filter(
+    lambda x: x['name'] == KIVY_DEFAULT_FONT, KIVY_FONTS)[0]['fn_regular']
 
 KIVY_ICONIC_FONT = "WebAwesome"
-KIVY_ICONIC_FONT_PATH = filter(lambda x: x['name'] == KIVY_ICONIC_FONT, KIVY_FONTS)[0]['fn_regular']
+KIVY_ICONIC_FONT_PATH = filter(
+    lambda x: x['name'] == KIVY_ICONIC_FONT, KIVY_FONTS)[0]['fn_regular']
 
 COLOR1 = get_color_from_hex('E62E25')
-COLOR_RED = (1,.85,.85,1)
-COLOR_BLUE = (.85,.85,1,1)
+COLOR_RED = (1, .85, .85, 1)
+COLOR_BLUE = (.85, .85, 1, 1)
--- pomodoro.py
+++ pomodoro.py
@@ -6,42 +6,48 @@ from kivy.core.window import Window
 from kivy.vector import Vector
 from kivy.properties import NumericProperty, StringProperty
 from kivy.clock import Clock
-from time import strftime, gmtime
 from kivy.animation import Animation
 from kivy.core.audio import SoundLoader
 from kivy.config import Config
+from kivy.uix.screenmanager import SlideTransition
+from time import strftime, gmtime
 from config import *
+from datetime import datetime
 
 Config.set('graphics', 'fullscreen', 0)
 Config.set('graphics', 'resizable', 0)
+
 
 class Pomodoro(BoxLayout):
     time_period = NumericProperty()
     time_display = StringProperty()
     rotation = NumericProperty(0)
     start_time = NumericProperty()
+    sprint_count = NumericProperty(0)
+    state = StringProperty()
+    start = StringProperty()
+    stop = StringProperty()
 
-    def __init__(self, time_period, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(Pomodoro, self).__init__(*args, **kwargs)
-        self.time_period = time_period
-        self.time_display = strftime('%M:%S', gmtime(self.time_period))
+        self.set_to_workstate()
         self.animation = None
-        self.start_time = int(strftime('%M:%S', gmtime(self.time_period)).split(":")[0])
         self.clock = SoundLoader.load('assets/clock.wav')
         self.alarm = SoundLoader.load('assets/alarm.wav')
-        self.perrotation = Vector(1, 1).angle(
-            Vector(1, 1).rotate(360 / float(self.time_period)))
+        self.sm.current = 'start'
 
         # Default displayed numbers as minutes added within rule.
-        for i in range(1, 5):
-            pos = Vector(0, 70).rotate(self.perrotation * (self.time_period / 5) * i)
-            display_time = int(strftime('%M:%S', gmtime((self.time_period / 5) * i)).split(":")[0])
-            label = Label(text="[color=000000]%s min.[/color]"%display_time,
-                          markup=True, pos=pos, size=(10,10),
-                          font_name=KIVY_DEFAULT_FONT)
-            self.floatlayout.add_widget(label)
+#        for i in range(0, 5):
+#            pos = Vector(0, 80).rotate(self.perrotation * (self.time_period / 5) * i)
+#            display_time = int(strftime('%M:%S', gmtime((self.time_period / 5) * i)).split(":")[0])
+#            label = Label(text="[color=000000]%s[/color]"%display_time,
+#                          markup=True, pos=pos, size=(10,10),
+#                          font_name=KIVY_DEFAULT_FONT)
+#            self.floatlayout.add_widget(label)
 
     def start_animation(self):
+        if self.state == "work":
+            self.start = str(datetime.now()).rsplit('.', 1)[0]
         Clock.schedule_once(lambda dt: self.decrease_minutes(), 1)
         self.start_button.disabled = True
 
@@ -59,11 +65,39 @@ class Pomodoro(BoxLayout):
 
             if self.animation:
                 self.animation.stop(self)
-            self.animation = Animation(
-                rotation=self.rotation, d=1)
+            self.animation = Animation(rotation=self.rotation, d=1)
             self.animation.start(self)
 
+            if self.state == "work" and self.time_period < 1:
+                self.stop = str(datetime.now()).rsplit('.', 1)[0]
+
             Clock.schedule_once(lambda dt: self.decrease_minutes(), 1)
+
+    def switch_screen(self, screen):
+        self.sm.transition = SlideTransition(direction='up')
+        self.sm.current = screen
+
+    def set_clock(self):
+        self.start_time = int(
+            strftime('%M:%S', gmtime(self.time_period)).split(":")[0])
+        self.perrotation = Vector(1, 1).angle(
+            Vector(1, 1).rotate(360 / float(self.time_period)))
+
+    def set_to_workstate(self):
+        self.state = "work"
+        self.time_period = WORK_TIME_PERIOD
+        self.time_display = strftime('%M:%S', gmtime(self.time_period))
+        self.sprint_count += 1
+        self.set_clock()
+        self.switch_screen('start')
+
+    def set_to_breakstate(self):
+        self.state = "break"
+        self.time_period = BREAK_TIME_PERIOD * self.sprint_count
+        self.sprint_count = 0
+        self.time_display = strftime('%M:%S', gmtime(self.time_period))
+        self.set_clock()
+        self.switch_screen('start')
 
     def reset_time(self):
         if self.animation:
@@ -71,9 +105,36 @@ class Pomodoro(BoxLayout):
         self.rotation = 0
         self.clock.stop()
         self.alarm.play()
-        self.time_period = WORK_TIME_PERIOD
-        self.time_display = strftime('%M:%S', gmtime(self.time_period))
         self.start_button.disabled = False
+        if self.state == "break":
+            self.switch_screen('start')
+            self.set_to_workstate()
+        else:
+            self.switch_screen('info')
+
+    def set_volume(self, state):
+        on_off = 0 if state == "down" else 1
+        if self.clock:
+            self.clock.volume = on_off
+        if self.alarm:
+            self.alarm.volume = on_off
+
+    def keep_info(self, text):
+        if text:
+            data = dict(date_from=self.start,
+                        date_to=self.stop,
+                        content=text,
+                        is_sent=False)
+            self.message.text = ""
+            current_day = self.start.rsplit(" ", 1)[0]
+            try:
+                existing_data = DB.store_get(current_day)
+            except KeyError:
+                existing_data = []
+            existing_data.append(data)
+            DB.store_put(current_day, existing_data)
+            DB.store_sync()
+            self.switch_screen('action')
 
 
 class PomodoroApp(App):
@@ -86,7 +147,7 @@ class PomodoroApp(App):
         Window.size = (200, 240)
         Window.fullscreen = 0
         Window.resizable = 0
-        return Pomodoro(WORK_TIME_PERIOD)
+        return Pomodoro()
 
 if __name__ == "__main__":
     PomodoroApp().run()
